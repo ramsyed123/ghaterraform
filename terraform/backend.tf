fa9334b39dcbbc62d59bb8e77d7611d64311f54e
ramsyed123 <rameez786cool@gmail.com>
2024-12-18 07:38:24 +0000

    de

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "bucketk8"
+    key            = "./terraform/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
